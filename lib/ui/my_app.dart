5cd446d06a7b321f1fef803cab1b84b4ba0d9d38
Jhonatan0911 <jpinalozano911@gmail.com>
2021-12-17 23:02:15 -0500

    cambios en main y controladores de permisos y gps

--- lib/ui/my_app.dart
+++ lib/ui/my_app.dart
@@ -1,59 +1,77 @@
 import 'package:f_202110_firebase/domain/controller/authentication_controller.dart';
 import 'package:f_202110_firebase/domain/controller/chat_controller.dart';
 import 'package:f_202110_firebase/domain/controller/firestore_controller.dart';
+import 'package:f_202110_firebase/domain/managements/theme_management.dart';
+import 'package:f_202110_firebase/domain/controller/theme_controller.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
 import 'firebase_central.dart';
+import 'package:f_202110_firebase/ui/theme/theme.dart';
 
-class MyApp extends StatelessWidget {
-  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+class App extends StatefulWidget {
+  const App({Key? key}) : super(key: key);
+
   @override
-  Widget build(BuildContext context) {
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false, //banner de modo debug
-      title: 'Login usando Firebase',
-      theme: ThemeData(
-        colorScheme: ColorScheme(
-          brightness: Brightness.light,
-          surface: Colors.purple,
-          primary: Colors.purple,
-          onPrimary: Colors.white,
-          primaryVariant: Colors.grey,
-          secondary: Colors.grey,
-          secondaryVariant: Colors.grey,
-          background: Colors.grey,
-          onBackground: Colors.white,
-          error: Colors.grey,
-          onError: Colors.grey,
-          onSecondary: Colors.grey,
-          onSurface: Colors.purple,
-        ),
-        primarySwatch: Colors.purple,
-      ),
-      home: Scaffold(
-          body: FutureBuilder(
-        future: _initialization,
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            print("error ${snapshot.error}");
-            return Wrong();
-          }
-          if (snapshot.connectionState == ConnectionState.done) {
-            Get.put(FirebaseController());
-            Get.put(AuthenticationController());
-            Get.put(ChatController());
-            return FirebaseCentral();
-          }
-
-          return Loading();
-        },
-      )),
-    );
-  }
+  _AppState createState() => _AppState();
 }
 
+class _AppState extends State<App> {
+  // Dependency injection: setting up state management
+  late final ThemeController controller = Get.put(ThemeController());
+  // Theme management
+  late final ThemeManager manager = ThemeManager();
+  bool isLoaded = false;
+
+  Future<void> initializeTheme() async {
+    controller.darkMode = await manager.storedTheme;
+    setState(() => isLoaded = true);
+  }
+
+  @override
+  void initState() {
+    ever(controller.reactiveDarkMode, (bool isDarkMode) {
+      manager.changeTheme(isDarkMode: isDarkMode);
+    });
+    initializeTheme();
+    super.initState();
+  }
+
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+
+  @override
+  Widget build(BuildContext context) {
+    return !isLoaded
+        ? const CircularProgressIndicator()
+        : GetMaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Login usando Firebase',
+            theme: MyTheme.ligthTheme,
+            darkTheme: MyTheme.darkTheme,
+            themeMode: ThemeMode.system,
+            home: Scaffold(
+                body: FutureBuilder(
+              future: _initialization,
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  print("error ${snapshot.error}");
+                  return Wrong();
+                }
+                if (snapshot.connectionState == ConnectionState.done) {
+                  Get.put(FirebaseController());
+                  Get.put(AuthenticationController());
+                  Get.put(ChatController());
+                  return FirebaseCentral();
+                }
+
+                return Loading();
+              },
+            )),
+          );
+  }
+}
 class Wrong extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
